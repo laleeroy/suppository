d298a65d87238fbd110ba7e5d30d0011477fc692
borntohonk <6264306+borntohonk@users.noreply.github.com>
2021-10-17 13:46:29 +0000

    Repurpose the suppository

--- README.md
+++ README.md
@@ -2,8 +2,31 @@ This version of suppository exists to feed files to a docker container.
 
 example usage:
 
-```
-docker run --name suppository --rm --env GITHUB_TOKEN=$GITHUB_TOKEN --volume $PWD/bundle_and_publish.sh:/bundle_and_publish.sh --detach borntohonk/suppository:latest /bin/bash -c "git clone https://$GITHUB_TOKEN@github.com/borntohonk/NeutOS Atmosphere && git clone https://github.com/switchbrew/libnx.git libnx && git clone https://github.com/borntohonk/suppository.git suppository && make -C libnx -j4 && make -C libnx install && make -C Atmosphere dist-no-debug -j4 && sh bundle_and_publish.sh"
+```sh
+ docker run --name suppository \
+ --rm \
+ --env GITHUB_TOKEN=$GITHUB_TOKEN \
+ --volume $PWD/libnx:/libnx \
+ --volume $PWD/hbmenu:/hbmenu \
+ --volume $PWD/hbl:/hbl \
+ --volume $PWD/suppository:/suppository \
+ --volume $PWD/suppository/bundle_and_publish.sh:/bundle_and_publish.sh \
+ --volume $PWD/Atmosphere:/Atmosphere \
+ borntohonk/suppository:latest /bin/bash -c \
+ "make -C libnx clean && \
+ make -C libnx -j8 && \
+ make -C libnx install && \
+ make -C hbmenu clean && \
+ make -C hbmenu nx -j8 && \
+ make -C hbl clean && \
+ make -C hbl -j8 && \
+ make -C Atmosphere clean && \
+ make -C Atmosphere dist-no-debug -j8 && \
+ sh bundle_and_publish.sh && \
+ make -C libnx clean && \
+ make -C hbmenu clean && \
+ make -C hbl clean && \
+ make -C Atmosphere clean"
 ```
 
 If you have any inquiries; file an issue with the github tracker.
@@ -11,6 +34,7 @@ If you have any inquiries; file an issue with the github tracker.
 pre-requisites: 
 * must be able to use a text-editor (at very least) to make alterations to deploy to another repository.
 * have a github access token with appropriate permissions as GITHUB_TOKEN enviroment variable
+* have hbl/hbmenu/libnx/suppository/your-fork checked out to ./hbl ./hbmenu ./libnx ./Suppository ./Atmosphere (recurse-submodules for suppository)
 
 ---
 Credits: [@borntohonk](https://github.com/borntohonk)
--- bundle_and_publish.sh
+++ bundle_and_publish.sh
@@ -1,48 +1,40 @@
 #!/bin/bash
 echo "$(curl -s "https://api.github.com/repos/CTCaer/hekate/releases/latest" | grep "tag_name" | head -1 | cut -c 17-21)" > hekate.version && \
-echo "$(curl -s "https://api.github.com/repos/switchbrew/nx-hbloader/releases/latest" | grep "tag_name" | head -1 | cut -c 17-21)" > hbl.version && \
-echo "$(curl -s "https://api.github.com/repos/switchbrew/nx-hbmenu/releases/latest" | grep "tag_name" | head -1 | cut -c 17-21)" > hbmenu.version && \
-echo "$(curl -s "https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases" | grep "tag_name" | head -1 | cut -c 18-22)" > ams.version && \
+HBLVER=$(grep -P "APP_VERSION\t:=\t" hbl/Makefile | head -1 | cut -c 16-20)
+HBMENUVER=$(grep -P "export APP_VERSION\t:=\t" hbmenu/Makefile | head -1 | cut -c 23-27)
+AMSMAJORVER=$(grep 'define ATMOSPHERE_RELEASE_VERSION_MAJOR\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3) && \
+AMSMINORVER=$(grep 'define ATMOSPHERE_RELEASE_VERSION_MINOR\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3) && \
+AMSMICROVER=$(grep 'define ATMOSPHERE_RELEASE_VERSION_MICRO\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3) && \
+HOS_MAJORVER=$(grep 'define ATMOSPHERE_SUPPORTED_HOS_VERSION_MAJOR\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3) && \
+HOS_MINORVER=$(grep 'define ATMOSPHERE_SUPPORTED_HOS_VERSION_MINOR\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3) && \
+HOS_MICROVER=$(grep 'define ATMOSPHERE_SUPPORTED_HOS_VERSION_MICRO\b' Atmosphere/libraries/libvapours/include/vapours/ams/ams_api_version.h | tr -s [:blank:] | cut -d' ' -f3) && \
 echo "$(curl -s https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases | grep "browser_download_url" | head -1 | grep -oE "\-([0-9a-f]{8})" | head -1 | cut -c2-)" > ams.hash && \
-echo "$(curl -s https://api.github.com/repos/borntohonk/NeutOS/releases | grep "browser_download_url" | head -1 | grep -oE "\-([0-9a-f]{8})" | head -1 | cut -c2-)" > neutos.hash && \
-AMSVER=`cat ams.version` && \
-HBLVER=`cat hbl.version` && \
 HEKATEVER=`cat hekate.version` && \
-HBMENUVER=`cat hbmenu.version` && \
-AMSHASH=`cat ams.hash` && \
-NEUTOSHASH=`cat neutos.hash` && \
+AMSVER=$AMSMAJORVER.$AMSMINORVER.$AMSMICROVER && \
+HOSVER=$HOS_MAJORVER.$HOS_MINORVER.$HOS_MICROVER && \
 mkdir ams && \
 mv $(find Atmosphere -iname \*.zip) ams/temp.zip && \
 unzip ams/temp.zip -d ams && \
 rm ams/temp.zip && \
-HBLVER=`cat hbl.version` && \
-HEKATEVER=`cat hekate.version` && \
-HBMENUVER=`cat hbmenu.version` && \
 AMSHASH=`cat ams.hash` && \
-mkdir hbl && \
-mkdir hbmenu && \
-mkdir hekate && \
-mkdir temppatches && \
 mkdir updater && \
-wget $(curl -s https://api.github.com/repos/switchbrew/nx-hbloader/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -O hbl/hbl.nsp && \
-wget $(curl -s https://api.github.com/repos/switchbrew/nx-hbmenu/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -O hbmenu/temp.zip && \
+mkdir ams/bootloader && \
+mkdir hekate && \
 wget $(curl -s https://api.github.com/repos/CTCaer/hekate/releases/latest | grep "browser_download_url" | head -1 | cut -d '"' -f 4) -O hekate/temp.zip && \
-wget $(curl -s https://api.github.com/repos/borntohonk/patches/releases/latest | grep "browser_download_url" | head -1 | cut -d '"' -f 4) -O temppatches/temp.zip && \
-wget $(curl -s https://api.github.com/repos/borntohonk/aio-neutos-updater/releases/latest | grep "browser_download_url" | head -1 | cut -d '"' -f 4) -O updater/temp.zip && \
-unzip hbmenu/temp.zip -d hbmenu && \
 unzip hekate/temp.zip -d hekate && \
-unzip temppatches/temp.zip -d temppatches && \
+rm hekate/temp.zip && \
+ls -1t suppository/patches/hekate_patches/*.ini | while read fn ; do cat "$fn" >> ams/bootloader/patches.ini; done && \
+cp -r suppository/patches/atmosphere ams/ && \
+wget $(curl -s https://api.github.com/repos/borntohonk/aio-neutos-updater/releases/latest | grep "browser_download_url" | head -1 | cut -d '"' -f 4) -O updater/temp.zip && \
 unzip updater/temp.zip -d updater && \
 cp -r updater/switch ams/
-rm temppatches/temp.zip && \
 mkdir ams/atmosphere/hosts && \
 cp hbl/hbl.nsp ams/atmosphere/hbl.nsp && \
-cp hbmenu/*.nro ams/hbmenu.nro && \
+cp hbmenu/hbmenu.nro ams/hbmenu.nro && \
 cp -r hekate/bootloader ams/ && \
 cp hekate/*.bin ams/payload.bin && \
+mv ams/atmosphere/reboot_payload.bin ams/bootloader/payloads/fusee.bin && \
 cp ams/payload.bin ams/atmosphere/reboot_payload.bin && \
-cp -r temppatches/atmosphere ams/ && \
-cp -r temppatches/bootloader ams/ && \
 cp suppository/tools/boot.dat ams/boot.dat && \
 cp suppository/tools/boot.ini ams/boot.ini && \
 cp suppository/configs/hekate_ipl.ini ams/bootloader/hekate_ipl.ini && \
@@ -57,10 +49,10 @@ echo "zip built, proceeding with publishing release"
 res=`curl --user "borntohonk:$GITHUB_TOKEN" -X POST https://api.github.com/repos/borntohonk/NeutOS/releases \
 -d "
 {
-  \"tag_name\": \"$AMSVER-$AMSHASH-neutos\",
+  \"tag_name\": \"$HOSVER-$AMSVER-$AMSHASH\",
   \"target_commitish\": \"master\",
-  \"name\": \"NeutOS $AMSVER-$AMSHASH-neutos\",
-  \"body\": \"![Banner](https://github.com/borntohonk/NeutOS/raw/neutos/img/banner.png)\r\n\r\n**NOTE: This is a forked variant, it is not unedited atmosphere.** \r\nThis bundle comes pre-configured for emummc usage.\r\n\r\nNeutos is a minor Atmosphere-fork, and cfw bundle maintained for myself.\r\n\r\nThere is an updater homebrew included now ( https://github.com/borntohonk/aio-neutos-updater )\r\n\r\nSigpatches are made and distributed at ( https://github.com/borntohonk/patches ), \r\nPlease file an issue with the github issue tracker, if there are any inquiries.\r\nThis github and release is automated, and was published with suppository ( https://github.com/borntohonk/suppository ) \",
+  \"name\": \"NeutOS $AMSVER-$AMSHASH for FW version $HOSVER\",
+  \"body\": \"![Banner](https://github.com/borntohonk/NeutOS/raw/neutos/img/banner.png)\r\n\r\n- This release supports FW version ${HOSVER}, and sigpatches for loader, es, fs and nifm are included for this FW version. \r\n\r\n**- NOTE: This is a forked variant, it is not unedited atmosphere.** \r\n\r\n- This bundle comes pre-configured for emummc usage.\r\n\r\n- Neutos is a minor Atmosphere-fork, and cfw bundle maintained for myself.\r\n\r\n- There is an updater homebrew included now ( https://github.com/borntohonk/aio-neutos-updater )\r\n\r\n- Sigpatches are made and distributed at ( https://github.com/borntohonk/patches ), \r\n\r\nPlease file an issue with the github issue tracker, if there are any inquiries.\r\n\r\nThis github and release is automated, and was published with suppository ( https://github.com/borntohonk/suppository ) \",
   \"draft\": false,
   \"prerelease\": false
 }"`
