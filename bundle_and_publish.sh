dcf168b096f4847595c736e237d596f28e3fc427
borntohonk <6264306+borntohonk@users.noreply.github.com>
2022-01-20 11:46:56 +0000

    Change default to be fusee.bin, as this bundle doesn't really need it

--- bundle_and_publish.sh
+++ bundle_and_publish.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "$(curl -s https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases | grep "browser_download_url" | head -1 | grep -oE "\-([0-9a-f]{8})" | head -1 | cut -c2-)" > ams.hash && \
 echo "$(curl -s https://api.github.com/repos/borntohonk/NeutOS/releases | grep "browser_download_url" | head -1 | grep -oE "\-([0-9a-f]{8})" | head -1 | cut -c2-)" > neutos.hash && \
-echo "$(curl -s "https://api.github.com/repos/CTCaer/hekate/releases/latest" | grep "tag_name" | head -1 | cut -c 17-21)" > hekate.version && \
 AMSHASH=`cat ams.hash` && \
 NEUTOSHASH=`cat neutos.hash` && \
 if [ "$AMSHASH" = "$NEUTOSHASH" ]; then
@@ -22,7 +21,6 @@ else
  HBMENUVER=$(grep -P "export APP_VERSION\t:=\t" hbmenu/Makefile | head -1 | cut -c 23-27)
  HOSVER=$HOS_MAJORVER.$HOS_MINORVER.$HOS_MICROVER
  AMSVER=$AMSMAJORVER.$AMSMINORVER.$AMSMICROVER
- HEKATEVER=`cat hekate.version` && \
  AMSVER=$AMSMAJORVER.$AMSMINORVER.$AMSMICROVER && \
  HOSVER=$HOS_MAJORVER.$HOS_MINORVER.$HOS_MICROVER && \
  mkdir ams && \
@@ -32,17 +30,10 @@ else
  rm ams/temp.zip && \
  AMSHASH=`cat ams.hash` && \
  mkdir updater && \
- mkdir ams/bootloader && \
- mkdir hekate && \
- wget $(curl -s https://api.github.com/repos/CTCaer/hekate/releases/latest | grep "browser_download_url" | head -1 | cut -d '"' -f 4) -O hekate/temp.zip && \
- sleep 10 && \
- unzip hekate/temp.zip -d hekate && \
- rm hekate/temp.zip && \
  cd SigPatches && \
  python3 scripts/loader_patch.py && \
  cd ../ && \
  sleep 10 && \
- cp -r SigPatches/SigPatches/bootloader ams && \
  cp -r SigPatches/SigPatches/atmosphere ams && \
  wget $(curl -s https://api.github.com/repos/borntohonk/aio-neutos-updater/releases/latest | grep "browser_download_url" | head -1 | cut -d '"' -f 4) -O updater/temp.zip && \
  sleep 10 && \
@@ -58,21 +49,13 @@ else
  sleep 10 && \
  unzip hbmenu/temp.zip -d hbmenu && \
  cp hbmenu/*/*.nro ams/hbmenu.nro && \
- cp -r hekate/bootloader ams/ && \
- cp hekate/*.bin ams/payload.bin && \
- mv ams/atmosphere/reboot_payload.bin ams/bootloader/payloads/fusee.bin && \
- wget $(curl -s https://api.github.com/repos/shchmue/Lockpick_RCM/releases/latest | grep "browser_download_url" | head -1 | cut -d '"' -f 4) -O ams/bootloader/payloads/Lockpick_RCM.bin && \
- sleep 10 && \
- wget $(curl -s https://api.github.com/repos/suchmememanyskill/TegraExplorer/releases/latest | grep "browser_download_url" | head -1 | cut -d '"' -f 4) -O ams/bootloader/payloads/TegraExplorer.bin && \
- sleep 10 && \
- cp ams/payload.bin ams/atmosphere/reboot_payload.bin && \
- cp configs/hekate_ipl.ini ams/bootloader/hekate_ipl.ini && \
+ cp ams/atmosphere/reboot_payload.bin ams/payload.bin && \
  cp configs/emummc.txt ams/atmosphere/hosts/emummc.txt && \
  cp configs/sysmmc.txt ams/atmosphere/hosts/sysmmc.txt && \
  cp configs/exosphere.ini ams/exosphere.ini && \
  mkdir out && \
  cd ams && \
- zip -r ../out/NeutOS-${AMSVER}-master-${AMSHASH}+hbl-${HBLVER}+hbmenu-${HBMENUVER}+hekate-${HEKATEVER}+patches.zip ./* && \
+ zip -r ../out/NeutOS-${AMSVER}-master-${AMSHASH}+hbl-${HBLVER}+hbmenu-${HBMENUVER}+patches.zip ./* && \
  cd .. && \
  echo "zip built, proceeding with publishing release"
  res=`curl --user "$GITHUB_USER:$GITHUB_TOKEN" -X POST https://api.github.com/repos/borntohonk/NeutOS/releases \
@@ -81,7 +64,7 @@ else
    \"tag_name\": \"$HOSVER-$AMSVER-$AMSHASH\",
    \"target_commitish\": \"master\",
    \"name\": \"NeutOS $AMSVER-$AMSHASH for FW version $HOSVER\",
-   \"body\": \"![Banner](https://github.com/borntohonk/NeutOS/raw/neutos/img/banner.png)\r\n\r\n- This release supports FW version ${HOSVER}, and sigpatches for loader, es, fs and nifm are included for this FW version. \r\n\r\n- This bundle comes pre-configured for emummc usage.\r\n\r\n- Neutos is a minor Atmosphere-fork, and cfw bundle maintained for myself.\r\n\r\n- There is an updater homebrew included now ( https://github.com/borntohonk/aio-neutos-updater )\r\n\r\n- Sigpatches are made and distributed at ( https://github.com/borntohonk/SigPatches ), \r\n\r\nPlease file an issue with the github issue tracker, if there are any inquiries.\r\n\r\nThis github and release is automated, and was published with suppository ( https://github.com/borntohonk/suppository ) \",
+   \"body\": \"![Banner](https://github.com/borntohonk/NeutOS/raw/neutos/img/banner.png)\r\n\r\n- This release supports FW version ${HOSVER}, and sigpatches for loader, es, fs and nifm are included for this FW version. \r\n\r\n- The payload.bin included is the latest Fusee payload from Atmosphere. \r\n\r\n- Neutos is an Atmosphere cfw bundle maintained for myself. \r\n\r\n- There is an updater homebrew included now ( https://github.com/borntohonk/aio-neutos-updater )\r\n\r\n- Sigpatches are made and distributed at ( https://github.com/borntohonk/SigPatches ), \r\n\r\nPlease file an issue with the github issue tracker, if there are any inquiries.\r\n\r\nThis github and release is automated, and was published with suppository ( https://github.com/borntohonk/suppository ) \",
    \"draft\": false,
    \"prerelease\": false
  }"`
